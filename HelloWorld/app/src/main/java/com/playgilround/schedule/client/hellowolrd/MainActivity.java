f6778dbb15df62e01990abbb2af78c5ddd3ce476
hyunwook <c004245@naver.com>
2019-03-27 01:50:28 +0900

    Add ButterKnife, AndroidX Use Lambda

--- HelloWorld/app/src/main/java/com/playgilround/schedule/client/hellowolrd/MainActivity.java
+++ HelloWorld/app/src/main/java/com/playgilround/schedule/client/hellowolrd/MainActivity.java
@@ -1,32 +1,26 @@
 package com.playgilround.schedule.client.hellowolrd;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import rx.Observable;
-import rx.Subscriber;
-import rx.functions.Action1;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.tvMain)
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        ButterKnife.bind(this);
+        
         Observable
                 .just("Hello RxAndroid..~")
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        ((TextView) findViewById(R.id.tvMain)).setText(s);
-                    }
-                });
+                .subscribe(s -> tvMain.setText(s));
     }
 }
