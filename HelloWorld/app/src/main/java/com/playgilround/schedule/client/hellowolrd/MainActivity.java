f31776dff5827ffb2b638b55c599ac49047ce58b
hyunwook <c004245@naver.com>
2019-05-09 23:12:23 +0900

    test RxBinding, RxView

--- HelloWorld/app/src/main/java/com/playgilround/schedule/client/hellowolrd/MainActivity.java
+++ HelloWorld/app/src/main/java/com/playgilround/schedule/client/hellowolrd/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
+
+import java.util.Random;
+
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "Text here..." + text);
             return text.toUpperCase();
         }).subscribe(s -> tvMain.setText(s));
+
+        RxView.clicks(findViewById(R.id.btnRx))
+                .map(event -> new Random().nextInt())
+                .subscribe(value -> {
+                    TextView textView = findViewById(R.id.tvRxView);
+                    textView.setText("number ->" + value.toString());
+                }, throwable -> {
+                    Log.d(TAG, "Error -> " +throwable.getMessage());
+                    throwable.printStackTrace();
+                });
     }
 }
 
