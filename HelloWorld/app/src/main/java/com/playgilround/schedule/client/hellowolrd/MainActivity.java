74a48649837cc9926b236971ec443c45ccd04765
hyunwook <c004245@naver.com>
2019-05-09 22:24:02 +0900

    base Observable, Subscriber

--- HelloWorld/app/src/main/java/com/playgilround/schedule/client/hellowolrd/MainActivity.java
+++ HelloWorld/app/src/main/java/com/playgilround/schedule/client/hellowolrd/MainActivity.java
@@ -1,26 +1,51 @@
 package com.playgilround.schedule.client.hellowolrd;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.tvMain)
     TextView tvMain;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        
-        Observable
-                .just("Hello RxAndroid..~")
-                .subscribe(s -> tvMain.setText(s));
+
+        Observable<String> simpleObservable =
+                Observable.create(subscriber -> {
+                    subscriber.onNext("RxAndroid Test.");
+                    subscriber.onCompleted();
+                });
+
+        simpleObservable
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "onCompleted.");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "Error -->" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        tvMain.setText(s);
+                    }
+                });
     }
 }
